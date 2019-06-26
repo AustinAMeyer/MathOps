e4f87d8fbc7b7edd5aaca6c426fbcc6b69084a0f
Austin <austin@tag219877.lrc.txstate.edu>
2019-06-26 12:41:38 -0500

    Initial commit

---
+++ MathOps/main.m
@@ -0,0 +1,51 @@
+//
+//  main.m
+//  MathOps
+//
+//  Created by Austin on 6/25/19.
+//  Copyright © 2019 Austin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    
+    
+    /*
+    int apples = 3 + 5 + 4 + 1;
+    
+    int oranges = 10 - 3;
+    
+    int totalFruit = apples + oranges;
+    
+    int eggsPerCarton = 12;
+    
+    int eggs = eggsPerCarton * 4;
+    
+    int baskets = 4;
+    
+    float itemsPerBasket = (apples + oranges + eggs) / baskets;
+    */
+    
+    /*
+     > Greater than
+     < Less than
+     >= Greater than equal
+     <= Less than equal
+     == equal to
+     != not equal to
+     
+     */
+    
+    bool compareResult;
+    compareResult = 5 != 5.75;
+    
+    
+    
+    
+    return 0;
+}
